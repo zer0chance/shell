5d9435babb8e9363867f76519e40602b6509e31d
zer0chance <ier7928@gmail.com>
2020-01-18 19:03:47 +0700

    Create new program launching routine

--- myShell.c
+++ myShell.c
@@ -76,6 +76,35 @@ char** shell_split_line(char* line)
 }	
 
 
+int* shell_launche(char** args)
+{
+    pid_t = pid, wpid;
+    int status;
+
+    pid = fork();
+    if (pid == 0)
+    {
+        if (execvp(args[0], args) == -1)
+        {
+            perror("shell");
+            exit(EXIT_FAILURE);
+        }
+    } 
+    else if (pid < 0) 
+    {
+        perror("shell");
+    }   
+    else 
+    {
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 1;
+}
+
+
 void shell_loop()
 {
     char* line;
