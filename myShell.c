259c16f90ec15eae8b0f86475d7f84254eeb132c
zer0chance <ier7928@gmail.com>
2020-01-18 20:13:33 +0700

    Add cd, help and exit builtins

--- myShell.c
+++ myShell.c
@@ -5,6 +5,11 @@
 #define SHELL_TOK_DELIM " \n\t\a\r"
 
 
+int shell_cd(char **args);
+int shell_help(char **args);
+int shell_exit(char **args);
+
+
 char* shell_read_line()
 {
     int bufsize = SHELL_BUFSIZE;
@@ -102,6 +107,53 @@ int* shell_launche(char** args)
     }
 
     return 1;
+}
+
+
+char *builtin_str[] = {
+  "cd",
+  "help",
+  "exit"
+};
+
+int (*builtin_func[]) (char **) = {
+  &shell_cd,
+  &shell_help,
+  &shell_exit
+};
+
+int shell_num_builtins() {
+  return sizeof(builtin_str) / sizeof(char *);
+}
+
+int shell_cd(char **args)
+{
+  if (args[1] == NULL) {
+    fprintf(stderr, "shell: expected argument to \"cd\"\n");
+  } else {
+    if (chdir(args[1]) != 0) {
+      perror("shell");
+    }
+  }
+  return 1;
+}
+
+int shell_help(char **args)
+{
+  printf("Type program names and arguments, and hit enter.\n");
+  printf("The following are built in:\n");
+
+  for (int i = 0; i < shell_num_builtins(); i++) {
+    printf("  %s\n", builtin_str[i]);
+  }
+
+  printf("Use the man command for information on other programs.\n");
+  return 1;
+}
+
+int shell_exit(char **args)
+{
+  return 0;
 }
 
 
