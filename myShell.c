5ea7d60c7a05a775cfcbc51438254ffbf376a475
zer0chance <ier7928@gmail.com>
2020-01-18 20:18:26 +0700

    Create command execute function

--- myShell.c
+++ myShell.c
@@ -157,6 +157,23 @@ int shell_exit(char **args)
 }
 
 
+int shell_execute(char **args)
+{
+  if (args[0] == NULL) {
+    // An empty command was entered.
+    return 1;
+  }
+
+  for (int i = 0; i < shell_num_builtins(); i++) {
+    if (strcmp(args[0], builtin_str[i]) == 0) {
+      return (*builtin_func[i])(args);
+    }
+  }
+
+  return shell_launch(args);
+}
+
+
 void shell_loop()
 {
     char* line;
